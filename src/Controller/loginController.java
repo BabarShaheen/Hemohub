056477b98e4185ac767406c9aad0dc0d2c18e40f
BabarShaheen <i210847@nu.edu.pk>
2024-05-09 01:32:05 +0500

    login

--- src/Controller/loginController.java
+++ src/Controller/loginController.java
@@ -2,6 +2,9 @@ package Controller;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
@@ -10,11 +13,14 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import sample.DatabaseConnection;
 
+import javax.swing.*;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class loginController  {
+    public Button signupButton;
     @FXML
     private Button closeButton;
     @FXML
@@ -29,10 +35,26 @@ public class loginController  {
     public void loginButtonOnAction(ActionEvent e) {
         validateLogin();
     }
+    public void signupButtonOnAction(ActionEvent e) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/signup.fxml"));
+            Parent root = loader.load();
+            signupController signupController = loader.getController();
+
+            // Get the current stage
+            Stage stage = (Stage) signupButton.getScene().getWindow();
+
+            // Set the scene to the signup page
+            stage.setScene(new Scene(root));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
     public void validateLogin(){
         DatabaseConnection connectNow = new DatabaseConnection();
         Connection connectDB = connectNow.getConnection();
-        String verifyLoginQuery = "Select count(1) FROM admin where name = '" + usernameTextField.getText() + " ' and password = '" + passwordPasswordField.getText() +"'";
+        String verifyLoginQuery = "Select count(1) FROM admin where name = '" + usernameTextField.getText() + "' and password = '" + passwordPasswordField.getText() +"'";
         try {
             Statement statement = connectDB.createStatement();
             ResultSet queryResult = statement.executeQuery(verifyLoginQuery);
@@ -45,17 +67,15 @@ public class loginController  {
                 }
                 else {
                     loginLabel.setText("Invalid Login. Please try again");
+
                 }
 
             }
 
         } catch(Exception e){
-
+            e.printStackTrace();
         }
-
     }
-
-
 
     public void closeButtonOnAction(ActionEvent e){
         Stage stage = (Stage) closeButton.getScene().getWindow();
