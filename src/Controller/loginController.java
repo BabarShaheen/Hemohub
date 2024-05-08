1ac8647dce093942de1877f5277c9dd06969c805
Usman Afzal <muhusmanafzal@gmail.com>
2024-05-09 00:23:57 +0500

    established database connection. (change databasePassword and databaseUser to own)

--- src/Controller/loginController.java
+++ src/Controller/loginController.java
@@ -8,6 +8,11 @@ import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
+import sample.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class loginController  {
     @FXML
@@ -20,17 +25,37 @@ public class loginController  {
     private TextField usernameTextField;
     @FXML
     private PasswordField passwordPasswordField;
-    public void loginButtonOnAction(ActionEvent e){
-        if(usernameTextField.getText().isBlank()  && passwordPasswordField.getText().isBlank())
-        {
-            loginLabel.setText("Enter username and password first");
-        }
-        else {
-            loginLabel.setText("Login Button Clicked");
-        }
+    @FXML
+    public void loginButtonOnAction(ActionEvent e) {
+        validateLogin();
+    }
+    public void validateLogin(){
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+        String verifyLoginQuery = "Select count(1) FROM admin where name = '" + usernameTextField.getText() + " ' and password = '" + passwordPasswordField.getText() +"'";
+        try {
+            Statement statement = connectDB.createStatement();
+            ResultSet queryResult = statement.executeQuery(verifyLoginQuery);
 
+            while(queryResult.next())
+            {
+                if(queryResult.getInt(1)==1)
+                {
+                    loginLabel.setText("Welcome!");
+                }
+                else {
+                    loginLabel.setText("Invalid Login. Please try again");
+                }
+
+            }
+
+        } catch(Exception e){
+
+        }
 
     }
+
+
 
     public void closeButtonOnAction(ActionEvent e){
         Stage stage = (Stage) closeButton.getScene().getWindow();
