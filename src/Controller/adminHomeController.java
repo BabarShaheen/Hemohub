933adb33a2ef3c6e9d79f400a2b495c622441a2f
BabarShaheen <i210847@nu.edu.pk>
2024-05-11 21:08:23 +0500

    implemented patient Functionality

--- src/Controller/adminHomeController.java
+++ src/Controller/adminHomeController.java
@@ -7,12 +7,17 @@ import Classes.User;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import javax.swing.*;
+import java.io.IOException;
 import java.time.LocalDate;
 
 public class adminHomeController {
@@ -112,6 +117,22 @@ public class adminHomeController {
     public void manageInventoryButtonOnAction(ActionEvent actionEvent) {
     }
 
-    public void logoutButtonOnAction(ActionEvent actionEvent) {
+    public void logoutButtonOnAction(ActionEvent e) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/login.fxml"));
+            Parent root = loader.load();
+
+            loginController controller = loader.getController();
+            if (controller == null) {
+                throw new RuntimeException("Failed to get controller from FXML loader");
+            }
+
+
+            Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
